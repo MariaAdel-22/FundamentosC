c244ebe38fd0a9a7b7326b484a30db96f402dbd1
MariaAdel-22 <maria.espana1@educamadrid.org>
2021-11-22 11:26:46 +0100

    com1

--- Formulario/Formulario/Form1.Designer.cs
+++ Formulario/Formulario/Form1.Designer.cs
@@ -29,13 +29,58 @@ namespace Formulario
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(236, 149);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(96, 39);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Pulsame";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(196, 89);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(71, 15);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Soy un label";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(273, 86);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(166, 23);
+            this.txt1.TabIndex = 2;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txt1;
     }
 }
 
--- Formulario/Formulario/Form1.cs
+++ Formulario/Formulario/Form1.cs
@@ -19,5 +19,30 @@ namespace Formulario
             this.Text = "Primer Form App";
         }
 
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //Propiedades más comunes: la propiedad de tipo primitivo (int,string,...)
+            this.txt1.Width = 250;
+            this.btn1.Text = "Pulsando...";
+
+            //Tenemos propiedades de tipo objeto, en dichas propiedades debemos instanciar un objeto y adaptarnos a lo que nos pide
+            this.btn1.Location = new Point(20,50);
+
+            //Existen algunas clases (rara excepcion) que no se crean pero se utilizan, dichas clases están cerradas, es que no existe mas formas de crear su valor y nos lo ofrecen directamente
+            this.btn1.BackColor = Color.Yellow;
+
+            //Propiedades enumeradas: Una propiedad enumerada me ofrece el valor directamente de forma textual, internamente tiene un número int.
+
+            this.txt1.TextAlign = HorizontalAlignment.Center;
+
+            int numeroM = 999;
+            short numeroMenor = 888;
+            numeroMenor = (short)numeroM;
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
