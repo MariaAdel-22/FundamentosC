c7d08c3de23312712c3c12f083959548af803cea
MariaAdel-22 <maria.espana1@educamadrid.org>
2021-11-22 14:10:18 +0100

    com4

---
+++ Formulario/Formulario/form04DiaNacimientoSemana.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Formulario
+{
+    partial class form04DiaNacimientoSemana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAnyo = new System.Windows.Forms.TextBox();
+            this.btnFecha = new System.Windows.Forms.Button();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.Location = new System.Drawing.Point(51, 58);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(36, 21);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Dia:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl2.Location = new System.Drawing.Point(51, 112);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(42, 21);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Mes:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl3.Location = new System.Drawing.Point(51, 177);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(41, 21);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Año:";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(103, 58);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(121, 23);
+            this.txtDia.TabIndex = 3;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(103, 110);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(121, 23);
+            this.txtMes.TabIndex = 4;
+            // 
+            // txtAnyo
+            // 
+            this.txtAnyo.Location = new System.Drawing.Point(103, 177);
+            this.txtAnyo.Name = "txtAnyo";
+            this.txtAnyo.Size = new System.Drawing.Size(121, 23);
+            this.txtAnyo.TabIndex = 5;
+            // 
+            // btnFecha
+            // 
+            this.btnFecha.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFecha.Location = new System.Drawing.Point(319, 70);
+            this.btnFecha.Name = "btnFecha";
+            this.btnFecha.Size = new System.Drawing.Size(156, 108);
+            this.btnFecha.TabIndex = 6;
+            this.btnFecha.Text = "Calcular dia";
+            this.btnFecha.UseVisualStyleBackColor = true;
+            this.btnFecha.Click += new System.EventHandler(this.btnFecha_Click);
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResul.Location = new System.Drawing.Point(221, 271);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(48, 30);
+            this.lblResul.TabIndex = 7;
+            this.lblResul.Text = "DIA";
+            // 
+            // form04DiaNacimientoSemana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 340);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.btnFecha);
+            this.Controls.Add(this.txtAnyo);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "form04DiaNacimientoSemana";
+            this.Text = "form04DiaNacimientoSemana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAnyo;
+        private System.Windows.Forms.Button btnFecha;
+        private System.Windows.Forms.Label lblResul;
+    }
+}
---
+++ Formulario/Formulario/form04DiaNacimientoSemana.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class form04DiaNacimientoSemana : Form
+    {
+        public form04DiaNacimientoSemana()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFecha_Click(object sender, EventArgs e)
+        {
+
+            int dia = int.Parse(this.txtDia.Text);
+            int mes = int.Parse(this.txtMes.Text);
+            int anyo = int.Parse(this.txtAnyo.Text);
+
+            if (mes == 01) {
+
+                mes = 13;
+                anyo -= 1;
+
+            }else if (mes == 02) {
+
+                mes = 14;
+                anyo -= 1;
+            }
+
+            int op1 = ((mes + 1) * 3) / 5;
+            int op2 = anyo / 4;
+            int op3 = anyo / 100;
+            int op4 = anyo / 400;
+
+            int op5 = dia + (mes * 2) + anyo + op1 + op2 - op3 + op4 + 2;
+            int op6 = op5 / 7;
+            int op7 = op5 - (op6 * 7);
+
+            switch (op7) {
+
+                case 0:
+                    this.lblResul.Text = "SABADO";
+                break;
+
+                case 1:
+                    this.lblResul.Text = "DOMINGO";
+                break;
+
+                case 2:
+                    this.lblResul.Text = "LUNES";
+                break;
+
+                case 3:
+                    this.lblResul.Text = "MARTES";
+                break;
+
+                case 4:
+                    this.lblResul.Text = "MIERCOLES";
+                break;
+                case 5:
+                    this.lblResul.Text = "JUEVES";
+                break;
+                case 6:
+                    this.lblResul.Text = "VIERNES";
+                break;
+            }
+        }
+    }
+}
