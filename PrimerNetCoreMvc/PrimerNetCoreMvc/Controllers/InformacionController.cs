39ae9464b7682332d4a42c03508d40288a7cb774
MariaAdel-22 <maria.espana1@educamadrid.org>
2021-12-14 13:12:57 +0100

    comm4

---
+++ PrimerNetCoreMvc/PrimerNetCoreMvc/Controllers/InformacionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCoreMvc.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista() {
+
+            ViewData["Nombre"] = "Alumno";
+            ViewData["Edad"] = 32;
+
+            return View();
+        }
+    }
+}
