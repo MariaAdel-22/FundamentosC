5f171ef8a7f725a7a9cf7b5163e723a5e950b5d2
MariaAdel-22 <maria.espana1@educamadrid.org>
2021-12-20 11:48:08 +0100

    comm4

---
+++ NetCoreLinqSQL/NetCoreLinqSQL/Controllers/EmpleadosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCoreLinqSQL.Data;
+using NetCoreLinqSQL.Models;
+
+namespace NetCoreLinqSQL.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        EmpleadosContext context;
+
+        public EmpleadosController() {
+
+            this.context = new EmpleadosContext();
+        }
+
+        public IActionResult TodosEmpleados() {
+
+            List<Empleado> empleados = this.context.GetEmpleados();
+
+            return View(empleados);
+        
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCoreLinqSQL/NetCoreLinqSQL/Data/EmpleadosContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using NetCoreLinqSQL.Models;
+
+namespace NetCoreLinqSQL.Data
+{
+    public class EmpleadosContext
+    {
+        private SqlDataAdapter ademp;
+        private DataTable tablaemp;
+
+        public EmpleadosContext() {
+
+            string cadenaconexion = @"Data Source=T06W05;Initial Catalog=hospital;Persist Security Info=True;User ID=SA;Password=MCSD2021";
+            
+            string sql = "SELECT * FROM EMP";
+            this.ademp = new SqlDataAdapter(sql,cadenaconexion);
+
+            this.tablaemp = new DataTable();
+            this.ademp.Fill(tablaemp);
+
+        }
+
+        public List<Empleado> GetEmpleados() {
+
+            List<Empleado> empleados = new List<Empleado>();
+
+            //Para recuperar datos con LINQ necesitamos hacerlo sobre la colección de filas de la tabla
+
+            var consulta = from datos in this.tablaemp.AsEnumerable() select datos;
+
+            //Ahora mismo en consulta tenemos una colección de filas(rows) que tiene cada fila un campo(field)
+
+            foreach (var row in consulta) {
+
+                Empleado emp = new Empleado();
+
+                //Recuperamos cada field del row
+
+                emp.IdEmpleado = row.Field<int>("EMP_NO");
+                emp.Apellido = row.Field<string>("APELLIDO");
+                emp.Oficio = row.Field<string>("OFICIO");
+                emp.Salario = row.Field<int>("SALARIO");
+                emp.IdDepartamento = row.Field<int>("DEPT_NO");
+
+                empleados.Add(emp);
+            }
+
+            return empleados;
+        }
+    }
+}
