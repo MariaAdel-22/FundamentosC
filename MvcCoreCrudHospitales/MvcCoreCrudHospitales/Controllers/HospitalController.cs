dae4524d4af5f059100d516b422ce4dd7910f490
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-01-12 12:23:02 +0100

    Comm3

---
+++ MvcCoreCrudHospitales/MvcCoreCrudHospitales/Controllers/HospitalController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCrudHospitales.Models;
+using MvcCoreCrudHospitales.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCrudHospitales.Controllers
+{
+    public class HospitalController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public HospitalController(RepositoryHospital repo) {
+
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Hospital> hospitales = this.repo.GetHospitales();
+
+            return View(hospitales);
+        }
+
+        public IActionResult Details(int id) {
+
+            Hospital hosp = this.repo.FindHospital(id);
+            return View(hosp);
+        }
+
+        public IActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Hospital hosp) {
+
+            this.repo.InsertarHospital(hosp.Nombre,hosp.Direccion,hosp.Telefono,hosp.Camas);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) {
+
+            this.repo.EliminarHospital(id);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id) {
+
+            Hospital hosp = this.repo.FindHospital(id);
+            return View(hosp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Hospital hosp) {
+
+            this.repo.ModificarHospital(hosp.IdHospital,hosp.Nombre,hosp.Direccion,hosp.Telefono,hosp.Camas);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreCrudHospitales/MvcCoreCrudHospitales/Repositories/RepositoryHospital.cs
@@ -0,0 +1,72 @@
+﻿using MvcCoreCrudHospitales.Data;
+using MvcCoreCrudHospitales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCrudHospitales.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context) {
+
+            this.context = context;
+        }
+
+        public List<Hospital> GetHospitales() {
+
+            var consulta = from datos in this.context.Hospitales select datos;
+
+            return consulta.ToList();
+        }
+
+        public Hospital FindHospital(int id) {
+
+            var consulta = from datos in this.context.Hospitales where datos.IdHospital == id select datos;
+            return consulta.First();
+        }
+
+        private int GetMaxId() {
+
+            int maxId = this.context.Hospitales.Max(x => x.IdHospital + 1);
+            return maxId;
+        }
+
+        public void InsertarHospital(string nombre, string direccion, string tel,int camas) {
+
+            Hospital hosp = new Hospital();
+
+            hosp.IdHospital = this.GetMaxId();
+            hosp.Nombre = nombre;
+            hosp.Direccion = direccion;
+            hosp.Telefono = tel;
+            hosp.Camas = camas;
+
+            this.context.Hospitales.Add(hosp);
+            this.context.SaveChanges();
+        }
+
+        public void EliminarHospital(int id) {
+
+            Hospital hosp = this.FindHospital(id);
+
+            this.context.Hospitales.Remove(hosp);
+            this.context.SaveChanges();
+        }
+
+        public void ModificarHospital(int id,string nombre, string direccion, string telefono, int camas) {
+
+            Hospital hosp = this.FindHospital(id);
+
+            hosp.Nombre = nombre;
+            hosp.Direccion = direccion;
+            hosp.Telefono = telefono;
+            hosp.Camas = camas;
+
+            this.context.SaveChanges();
+        }
+    }
+}
