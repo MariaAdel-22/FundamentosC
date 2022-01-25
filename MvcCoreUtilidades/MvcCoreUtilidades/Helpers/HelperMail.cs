2b717852600062408ca8b3060fb714cd1c2a3032
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-01-25 14:20:12 +0100

    coooom

---
+++ MvcCoreUtilidades/MvcCoreUtilidades/Helpers/HelperMail.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    public class HelperMail
+    {
+        private IConfiguration configuration;
+
+        public HelperMail(IConfiguration configuration) {
+
+            this.configuration = configuration;
+        }
+
+        //A veces puede venir la imagen o no en el mail, para ello, haremos una sobrecarga de métodos 
+    }
+}
---
+++ MvcCoreUtilidades/MvcCoreUtilidades/Helpers/HelperUploadFiles.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using MvcCoreUtilidades.Providers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    public class HelperUploadFiles
+    {
+        private PathProvider path;
+
+        public HelperUploadFiles(PathProvider path) {
+
+            this.path = path;
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile formfile, Folders folder) {
+
+            string filename = formfile.FileName;
+            string path = this.path.MapPath(filename, folder);
+
+            using (Stream stream = new FileStream(path, FileMode.Create)) {
+
+                await formfile.CopyToAsync(stream);
+            }
+
+            return path;
+        }
+
+    }
+}
