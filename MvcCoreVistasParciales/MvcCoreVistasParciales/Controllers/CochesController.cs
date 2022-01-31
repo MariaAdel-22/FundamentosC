087cf28598c0a8ffdc0603ca3d28f93d5bcc92da
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-01-31 09:42:12 +0100

    com1

---
+++ MvcCoreVistasParciales/MvcCoreVistasParciales/Controllers/CochesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreVistasParciales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreVistasParciales.Controllers
+{
+    public class CochesController : Controller
+    {
+
+        private List<Coche> Cars;
+
+        public CochesController() {
+
+            this.Cars = new List<Coche> {
+
+                new Coche{ IdCoche = 1, Marca="Pontiac", Modelo="Firebr", VelocidadMaxima=123},
+                new Coche{ IdCoche=2,Marca="Lamborgini",Modelo="Distint",VelocidadMaxima=140},
+                new Coche{IdCoche=3,Marca="Ferrari",Modelo="Testarrosa",VelocidadMaxima=300 },
+                new Coche{ IdCoche=4,Marca="Ford",Modelo="Mustang",VelocidadMaxima=290}
+            };
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(this.Cars);
+        }
+    }
+}
