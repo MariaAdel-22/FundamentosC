a90ee62ec36ba2e8717c5ee229d50f54e7aaf7a3
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-01-31 14:16:30 +0100

    com1

---
+++ MvcCoreVistasParciales/MvcCoreVistasParciales/Data/ContextCoches.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreVistasParciales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreVistasParciales.Data
+{
+    public class ContextCoches:DbContext
+    {
+        public ContextCoches(DbContextOptions<ContextCoches> option):base(option) { 
+        }
+
+        public DbSet<Coche> Coches { get; set; }
+    }
+}
---
+++ MvcCoreVistasParciales/MvcCoreVistasParciales/Repositories/RepositoryCoches.cs
@@ -0,0 +1,32 @@
+﻿using MvcCoreVistasParciales.Data;
+using MvcCoreVistasParciales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreVistasParciales.Repositories
+{
+    public class RepositoryCoches
+    {
+        public List<Coche> Cars;
+        public ContextCoches context;
+
+        public RepositoryCoches() {
+
+            this.Cars = new List<Coche> {
+
+                new Coche{ IdCoche = 1, Marca="Pontiac", Modelo="Firebr", VelocidadMaxima=123},
+                new Coche{ IdCoche=2,Marca="Lamborgini",Modelo="Distint",VelocidadMaxima=140},
+                new Coche{IdCoche=3,Marca="Ferrari",Modelo="Testarrosa",VelocidadMaxima=300 },
+                new Coche{ IdCoche=4,Marca="Ford",Modelo="Mustang",VelocidadMaxima=290}
+            };
+        }
+
+        public List<Coche> GetCoches() {
+
+            var consulta = from datos in this.context.Coches.AsEnumerable() select datos;
+            return consulta.ToList();
+        }
+    }
+}
