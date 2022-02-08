a6341e630fc4159c4f6c26f852b90ac8a0da6d49
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-02-08 11:14:04 +0100

    Com2

---
+++ MvcSeguridadPersonalizada/MvcSeguridadPersonalizada/Controllers/ManageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MvcSeguridadPersonalizada.Controllers
+{
+    public class ManageController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(string username,string password) {
+
+            if (username.ToLower() == "admin" && password.ToLower() == "admin") {
+
+                //Debemos crear una identidad (name y rol) y un principal. Dicha identidad debemos combinarla con la cookie de autentificacion
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+
+                /*Todo usuario puede contener una serie de características llamadas Claims. Podemos almacenarlas dentro del USER para usarlas a
+                 * lo largo de la APP*/
+
+                Claim claimUserName = new Claim(ClaimTypes.Name, username);
+                identity.AddClaim(claimUserName);
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal, new AuthenticationProperties
+                {
+                    //Le decimos el tiempo que va a mantener la sesión conectada
+                    ExpiresUtc = DateTime.Now.AddMinutes(45)
+                });
+
+                return RedirectToAction("Perfil", "Usuarios");
+            }
+            return View();
+        }
+    }
+}
---
+++ MvcSeguridadPersonalizada/MvcSeguridadPersonalizada/Filters/AuthorizeUsersAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUsersAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //Los usuarios son almacenados DENTRO de HttpContext y dentro de User
+            //Un usuario está compuesto por una identidad y un principal donde podemos saber el nombre del usuario o si está autenticado
+            var user = context.HttpContext.User;
+
+            if (user.Identity.IsAuthenticated == false) {
+
+                //Necesitamos realizar la redirección para llevarnos la petición a Login del controlador Manage
+                RouteValueDictionary rutalogin = new RouteValueDictionary(new
+                {
+                    controller="Manage",
+                    action="Login"
+                });
+
+                RedirectToRouteResult result = new RedirectToRouteResult(rutalogin);
+                context.Result = result;
+            }
+        }
+    }
+}
