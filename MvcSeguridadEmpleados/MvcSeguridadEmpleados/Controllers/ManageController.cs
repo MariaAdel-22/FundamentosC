5090db397698db2bdb6ce5ee38dd190516ab7c64
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-02-08 14:22:34 +0100

    com7

--- MvcSeguridadEmpleados/MvcSeguridadEmpleados/Controllers/ManageController.cs
+++ MvcSeguridadEmpleados/MvcSeguridadEmpleados/Controllers/ManageController.cs
@@ -1,22 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using MvcSeguridadEmpleados.Models;
+using MvcSeguridadEmpleados.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MvcSeguridadEmpleados.Controllers
 {
     public class ManageController : Controller
     {
+        private RepositoryEmpleado repo;
+
+        public ManageController(RepositoryEmpleado repo) {
+
+            this.repo = repo;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(string username,string password)
+        public async Task<IActionResult>Login(string username,string password)
         {
+            Empleado emp = this.repo.FindEmpleado(username, int.Parse(password));
+
+            if (emp != null)
+            {
+
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                Claim claimName = new Claim(ClaimTypes.Name,username);
+                identity.AddClaim(claimName);
+
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+
+                return RedirectToAction("PerfilEmpleado", "Empleados");
+            }
+            else {
+
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+            }
+
             return View();
+        }
+
+        public IActionResult ErrorAcceso() {
+
+            return View();        
         }
     }
 }
---
+++ MvcSeguridadEmpleados/MvcSeguridadEmpleados/Filters/AuthorizeEmpleadoAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSeguridadEmpleados.Filters
+{
+    public class AuthorizeEmpleadoAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+           
+        }
+    }
+}
