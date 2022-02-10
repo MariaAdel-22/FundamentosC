0b69f05a311a763e1d09d59f1094076be907682f
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-02-10 19:07:12 +0100

    com3

---
+++ MvcChat/MvcChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
