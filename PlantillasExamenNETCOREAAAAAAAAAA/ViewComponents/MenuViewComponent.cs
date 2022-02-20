fe40e472bdd826960fe46bd0ecc6a6d859f04b87
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2022-02-20 18:19:26 +0100

    Add files via upload

---
+++ PlantillasExamenNETCOREAAAAAAAAAA/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPracticaExamen.Models;
+using MvcPracticaExamen.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPracticaExamen.ViewComponents
+{
+    public class MenuViewComponent:ViewComponent
+    {
+        private PeliculasRepository repo;
+
+
+        public MenuViewComponent(PeliculasRepository repo) {
+
+            this.repo = repo;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+
+            List<Genero> Generos = this.repo.GetGeneros();
+            List<Nacionalidad> Nacionalidades = this.repo.GetNacionalidades();
+
+            ViewBag.Generos = Generos;
+            ViewBag.Nacionalidades = Nacionalidades;
+
+            return View();
+        }
+    }
+}
